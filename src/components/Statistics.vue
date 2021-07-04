898d4e1653ea786df434a77369f090352e51e4fa
Anton Kovalenko <CaribbeanLegend@mail.ru>
2021-07-05 09:45:03 +1000

    Изменить дизайн страницы статистики

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,29 +1,40 @@
 <template>
   <div class="statistics-screen">
-    <select>
-      <option>Сегодня</option>
-      <option>Вчера</option>
-      <option>Неделя</option>
-      <option>Месяц</option>
-      <option>Всего</option>
-    </select>
+    <div class="statistics-control">
+      <select>
+        <option>Сегодня</option>
+        <option>Вчера</option>
+        <option>Неделя</option>
+        <option>Месяц</option>
+        <option>Всего</option>
+      </select>
+      <button @click="exportData()">-></button>
+    </div>
     <div class="info-statistics">
-      <br>TODO Добавить возможность выгрузки статистики
-      <div class="info-statistics--summary">
-        <span>Всего записано человек</span><span>0</span>
-        <span>Оплачено услуг на сумму</span><span>0 руб.</span>
-      </div>
-      <div class="info-statistics--workers-title">Сотрудники</div>
-      <div v-for="worker in beautyshop?.workers" v-bind:key="worker.uuid" class="info-statistics--worker-block">
-        <div>{{ worker.fullName }}</div>
-        <div class="info-statistics--block">
-          <span>Записано человек</span><span>0</span>
-          <span>Оплачено услуг на сумму</span><span>0 руб.</span>
+      <div class="info-statistics--title">Общая информация</div>
+      <div class="info-statistics--block">
+        <div class="info-statistics--block-element">
+          <span>123</span>
+          <span>Человек записано</span>
+        </div>
+        <div class="info-statistics--block-element">
+          <span>0 руб.</span>
+          <span>Сумма услуг</span>
         </div>
       </div>
-    </div>
-    <div class="buttons-container buttons-container__single">
-      <button @click="goToList()">Экспорт</button>
+      <div v-for="worker in beautyshop?.workers" v-bind:key="worker.uuid" class="info-statistics--worker-block">
+        <div class="info-statistics--title">{{ worker.fullName }}</div>
+        <div class="info-statistics--block">
+          <div class="info-statistics--block-element">
+            <span>123</span>
+            <span>Человек записано</span>
+          </div>
+          <div class="info-statistics--block-element">
+            <span>0 руб.</span>
+            <span>Сумма услуг</span>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="buttons-container buttons-container__single">
       <button @click="goToList()">К списку записей</button>
@@ -44,12 +55,16 @@ export default defineComponent({
       return store.getters.getCurrentBeautyshop();
     });
     const goToList = () => {
-      router.push({name: 'CheckInList'/*, params: {uuid: '73b00c6d-a503-46b2-ae50-2bf609a82973'}*/});
+      router.push({name: 'CheckInList'});
     };
+    const exportData = () => {
+      console.log('Добавить экспортирование данных');
+    }
 
     return {
       beautyshop,
-      goToList
+      goToList,
+      exportData
     }
   }
 })
